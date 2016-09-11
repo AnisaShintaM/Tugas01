67d26b2f3fb4e9626275cfc7248a0fb5d17fdd02
Annisa Shinta Mahgfiroh <anisashintam@gmail.com>
2016-09-11 23:37:57 +0700

    scndcommit

---
+++ app/src/main/java/id/sch/smktelkom_mlg/tugas01/xirpl1001/tugas1/CheckBox.java
@@ -0,0 +1,7 @@
+package id.sch.smktelkom_mlg.tugas01.xirpl1001.tugas1;
+
+/**
+ * Created by user on 11/09/2016.
+ */
+public class CheckBox {
+}
--- app/src/main/java/id/sch/smktelkom_mlg/tugas01/xirpl1001/tugas1/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/tugas01/xirpl1001/tugas1/MainActivity.java
@@ -4,13 +4,17 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements CompoundButton.OnCheckedChangeListener {
     EditText etNama;
     Button bOk;
-    TextView tvHasil;
+    android.widget.CheckBox cb1;
+    android.widget.CheckBox cb2;
+    TextView tvHasil, tvBisa;
+    int nBisa;
 
 
     @Override
@@ -20,7 +24,12 @@ public class MainActivity extends AppCompatActivity {
 
         etNama = (EditText) findViewById(R.id.editTextnama);
         bOk = (Button) findViewById(R.id.buttonOK);
+        cb1 = (android.widget.CheckBox) findViewById(R.id.checkBoxmat);
+        cb2 = (android.widget.CheckBox) findViewById(R.id.checkBoxipa);
+        cb1.setOnCheckedChangeListener(this);
+        cb1.setOnCheckedChangeListener(this);
         tvHasil = (TextView) findViewById(R.id.textViewhasil);
+        tvBisa = (TextView) findViewById(R.id.textViewBisa);
 
         bOk.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -34,9 +43,21 @@ public class MainActivity extends AppCompatActivity {
 
     private void doClick() {
         String nama = etNama.getText().toString();
-        tvHasil.setText(nama);
+        String hasil = "Kelas Anda:\n";
+        int startlen = hasil.length();
+        if (cb1.isChecked()) hasil += cb1.getText() + "\n";
+        if (cb2.isChecked()) hasil += cb2.getText() + "\n";
+        if (hasil.length() == startlen) hasil += "Tidak ada pilihan";
+        tvHasil.setText("Selamat Anda terdaftar dengan nama " + nama + "Mengambil Kelas " + hasil);
     }
 
-}
 
+    @Override
+    public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+        if (isChecked) nBisa += 1;
+        else nBisa -= 1;
+
+        tvBisa.setText("Kelas " + nBisa + " Terpilih");
+    }
+}
 
